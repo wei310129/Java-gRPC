ac60396d25499a5ff462b016a3aa3cf4ae9490e1
AidenChen <wei310129@gmail.com>
2026-06-01 10:23:06 +0800

    first commit

---
+++ src/main/java/com/example/javagrpc/JavaGRpcApplication.java
@@ -0,0 +1,13 @@
+package com.example.javagrpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGRpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaGRpcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/javagrpc/JavaGRpcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javagrpc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGRpcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
