342902771db71bd91ea2b049d569e5ae8c37a15c
AidenChen <wei310129@gmail.com>
2026-06-01 16:39:53 +0800

    Add forwardSayHello method and gRPC client configuration to HelloServiceImpl

--- src/main/java/com/example/javagrpc/HelloServiceImpl.java
+++ src/main/java/com/example/javagrpc/HelloServiceImpl.java
@@ -4,13 +4,21 @@ import com.example.javagrpc.grpc.HelloRequest;
 import com.example.javagrpc.grpc.HelloResponse;
 import com.example.javagrpc.grpc.HelloServiceGrpc;
 import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
+import net.devh.boot.grpc.client.inject.GrpcClient;
 import net.devh.boot.grpc.server.service.GrpcService;
 
+@Slf4j
 @GrpcService
 public class HelloServiceImpl extends HelloServiceGrpc.HelloServiceImplBase {
 
+    @SuppressWarnings("unused")
+    @GrpcClient("hello-service-9091")
+    private HelloServiceGrpc.HelloServiceBlockingStub helloService9091;
+
     @Override
     public void sayHello(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
+        log.info("sayHello");
         HelloResponse response = HelloResponse.newBuilder()
                 .setMessage("Hello, " + request.getName())
                 .build();
@@ -26,6 +34,15 @@ public class HelloServiceImpl extends HelloServiceGrpc.HelloServiceImplBase {
                     .build();
             responseObserver.onNext(response);
         }
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void forwardSayHello(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
+        log.info("forwardSayHello");
+        // 呼叫 9091 的 SayHello，再把結果回傳給 caller
+        HelloResponse response = helloService9091.sayHello(request);
+        responseObserver.onNext(response);
         responseObserver.onCompleted();
     }
 }
